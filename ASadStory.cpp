9683ae0b1299d4ae246aa46ee14952e700eb3e4a
E Apricot <writeletter@163.com>
2017-10-30 17:32:47 +0800

    再次初始化提交
    其实不是初始化，就是之前的感觉太乱，删了，本地整理后重新再提交一次。
    另：Windows 10 自带的输入法可以在 GitHub Desktop 的文本框里面输入中文，自己安装的手心输入法就不行。

---
+++ ASadStory.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main(int argc, char **argv)
+{
+	long long ago;
+	double world;
+	unsigned letter;
+	short stay;
+	long memories;
+	std::cout << "I miss you.\n";
+	return 0;
+}
---
+++ AlignMalloc.cpp
@@ -0,0 +1,21 @@
+﻿#include <cstdlib>
+#include <cassert>
+
+void* align_malloc(size_t size, size_t align)
+{
+	assert(!(align & (align - (size_t)1u)));
+	char* rdata = (char*)malloc(size + sizeof(void*) + align);
+	if (!rdata)
+		return NULL;
+	size_t offset = (size_t)((char**)rdata) + sizeof(void*); // 2
+	char** adata = (char**)((offset | (align - 1u)) + 1u); // 2
+	adata[-1] = rdata;
+	return (void*)adata;
+}
+
+void align_free(void* adata, size_t align = 0u)
+{
+	char* rdata = ((char**)adata)[-1];
+	assert((rdata - (char*)adata) <= (ptrdiff_t)(sizeof(void*) + align));
+	free(rdata);
+}
